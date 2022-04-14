6e326835889a989231662c91b8f3bf0d6a2d2c57
Shivansh Vij <shivanshvij@outlook.com>
2022-04-14 16:34:44 -0400

    adding demo for certifier

--- cmd/main.go
+++ cmd/main.go
@@ -17,10 +17,17 @@
 package main
 
 import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
 	"flag"
+	"github.com/go-acme/lego/v4/certcrypto"
+	"github.com/go-acme/lego/v4/lego"
+	"github.com/go-acme/lego/v4/registration"
 	"github.com/loopholelabs/certifier"
 	"github.com/loopholelabs/certifier/pkg/storage/memory"
 	"github.com/loopholelabs/logging"
+	"time"
 )
 
 var (
@@ -28,6 +35,22 @@ var (
 	root   string
 	public string
 )
+
+type User struct {
+	Email        string                 `json:"email"`
+	Registration *registration.Resource `json:"registration"`
+	Key          *rsa.PrivateKey        `json:"key"`
+}
+
+func (u *User) GetEmail() string {
+	return u.Email
+}
+func (u *User) GetRegistration() *registration.Resource {
+	return u.Registration
+}
+func (u *User) GetPrivateKey() crypto.PrivateKey {
+	return u.Key
+}
 
 func main() {
 	logger := logging.NewConsoleLogger()
@@ -41,6 +64,47 @@ func main() {
 	flag.Parse()
 
 	c := certifier.New(root, public, certifier.WithLogger(logger), certifier.WithStorage(storage))
+
+	storage.SetCID("userid", "testcid")
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		panic(err)
+	}
+
+	acmeUser := &User{
+		Email: "testemail@testemail.com",
+		Key:   privateKey,
+	}
+
+	acmeConfig := lego.NewConfig(acmeUser)
+
+	acmeConfig.CADirURL = "https://acme-staging-v02.api.letsencrypt.org/directory"
+	acmeConfig.Certificate.KeyType = certcrypto.RSA4096
+
+	acmeClient, err := lego.NewClient(acmeConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	// This is an API Call to the ACME Directory, which can be slow
+	reg, err := acmeClient.Registration.Register(registration.RegisterOptions{TermsOfServiceAgreed: true})
+	if err != nil {
+		panic(err)
+	}
+
+	acmeUser.Registration = reg
+
+	go func() {
+		time.Sleep(time.Second * 15)
+		logger.Infof("Starting Cert Renewal, expecting TXT for _acme-challenge.testdomain.loopholelabs.com to point to testdomain-loopholelabs-com.testcid.%s", root)
+		cert, err := c.Renew("userid", "testdomain.loopholelabs.com", acmeClient, privateKey)
+		if err != nil {
+			panic(err)
+		}
+		logger.Infof("Certificate Retrieved Successfully: %+v\n", cert)
+		logger.Infof("Certificate Contents: %s\n", cert.Certificate)
+	}()
 
 	if err := c.Start(listen); err != nil {
 		panic(err)
